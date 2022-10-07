5648150b9fe0264afb4cd504091414605863b424
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-10-07 12:37:39 +0300

    clone bose speakers landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const formInputs = document.querySelectorAll('.form__input');
+
+document.querySelector('.form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of formInputs) {
+    input.value = '';
+  }
+}, false);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document.querySelector('input[name="tel"]')
+  .addEventListener('input', event => {
+    event.target.value = event.target.value
+      .split('').filter(c => !isNaN(+c)).join('');
+  });
